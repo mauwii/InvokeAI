24f836790e171fe0da41061e8e483a99cbb52df8
mauwii <Mauwii@outlook.de>
2023-01-27 23:08:09 +0100

    update globals.py

--- ldm/invoke/globals.py
+++ ldm/invoke/globals.py
@@ -1,4 +1,4 @@
-'''
+"""
 ldm.invoke.globals defines a small number of global variables that would
 otherwise have to be passed through long and complex call chains.
 
@@ -9,31 +9,31 @@ the attributes:
   - initfile       - path to the initialization file
   - try_patchmatch - option to globally disable loading of 'patchmatch' module
   - always_use_cpu - force use of CPU even if GPU is available
-'''
+"""
 
 import os
 import os.path as osp
-from pathlib import Path
 from argparse import Namespace
+from pathlib import Path
 from typing import Union
 
 Globals = Namespace()
 
 # This is usually overwritten by the command line and/or environment variables
-if os.environ.get('INVOKEAI_ROOT'):
-    Globals.root = osp.abspath(os.path.expanduser(os.environ.get('INVOKEAI_ROOT')))
-elif os.environ.get('VIRTUAL_ENV'):
-    Globals.root = osp.abspath(osp.join(os.environ.get('VIRTUAL_ENV'), '..'))
+if os.environ.get("INVOKEAI_ROOT"):
+    Globals.root = osp.abspath(os.path.expanduser(os.environ["INVOKEAI_ROOT"]))
+elif os.environ.get("VIRTUAL_ENV"):
+    Globals.root = osp.abspath(osp.join(os.environ["VIRTUAL_ENV"], ".."))
 else:
-    Globals.root = osp.abspath(osp.expanduser('~/invokeai'))
+    Globals.root = osp.abspath(osp.expanduser("~/invokeai"))
 
 # Where to look for the initialization file
-Globals.initfile = 'invokeai.init'
-Globals.models_file = 'models.yaml'
-Globals.models_dir = 'models'
-Globals.config_dir = 'configs'
-Globals.autoscan_dir = 'weights'
-Globals.converted_ckpts_dir = 'converted-ckpts'
+Globals.initfile = "invokeai.init"
+Globals.models_file = "models.yaml"
+Globals.models_dir = "models"
+Globals.config_dir = "configs"
+Globals.autoscan_dir = "weights"
+Globals.converted_ckpts_dir = "converted-ckpts"
 
 # Try loading patchmatch
 Globals.try_patchmatch = True
@@ -51,23 +51,29 @@ Globals.disable_xformers = False
 # whether we are forcing full precision
 Globals.full_precision = False
 
-def global_config_file()->Path:
+
+def global_config_file() -> Path:
     return Path(Globals.root, Globals.config_dir, Globals.models_file)
 
-def global_config_dir()->Path:
+
+def global_config_dir() -> Path:
     return Path(Globals.root, Globals.config_dir)
 
-def global_models_dir()->Path:
+
+def global_models_dir() -> Path:
     return Path(Globals.root, Globals.models_dir)
 
-def global_autoscan_dir()->Path:
+
+def global_autoscan_dir() -> Path:
     return Path(Globals.root, Globals.autoscan_dir)
 
-def global_set_root(root_dir:Union[str,Path]):
+
+def global_set_root(root_dir: Union[str, Path]):
     Globals.root = root_dir
 
-def global_cache_dir(subdir:Union[str,Path]='')->Path:
-    '''
+
+def global_cache_dir(subdir: Union[str, Path] = "") -> Path:
+    """
     Returns Path to the model cache directory. If a subdirectory
     is provided, it will be appended to the end of the path, allowing
     for huggingface-style conventions:
@@ -80,18 +86,19 @@ def global_cache_dir(subdir:Union[str,Path]='')->Path:
     One other caveat is that HuggingFace is moving some diffusers models
     into the "hub" subdirectory as well, so this will need to be revisited
     from time to time.
-    '''
-    home: str | None = os.getenv('HF_HOME')
+    """
 
-    if home is None:
-        home = os.getenv('XDG_CACHE_HOME')
+    if os.getenv("HF_HOME"):
+        home = osp.abspath(os.path.expanduser(os.environ["HF_HOME"]))
 
-        if home is not None:
-            # Set `home` to $XDG_CACHE_HOME/huggingface, which is the default location mentioned in HuggingFace Hub Client Library.
-            # See: https://huggingface.co/docs/huggingface_hub/main/en/package_reference/environment_variables#xdgcachehome
-            home += os.sep + 'huggingface'
+    elif os.getenv("XDG_CACHE_HOME"):
+        # Set `home` to $XDG_CACHE_HOME/huggingface, which is the default location mentioned in HuggingFace Hub Client Library.
+        # See: https://huggingface.co/docs/huggingface_hub/main/en/package_reference/environment_variables#xdgcachehome
+        home = osp.abspath(
+            osp.join(os.path.expanduser(os.environ["XDG_CACHE_HOME"]), "huggingface")
+        )
 
-    if home is not None:
-        return Path(os.path.expanduser(home),subdir)
     else:
-        return Path(Globals.root,'models',subdir)
+        home = osp.abspath(osp.join(Globals.root, "models"))
+
+    return Path(home, subdir)
